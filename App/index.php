1f80401fb1c0c298a79f01cf36ad3b07235810c1
Jorge Pabon <pianistapr@hotmail.com>
2024-05-29 14:31:16 -0400

    Forcing http -> https redirect
    In case the ingress controller doesn't do it, I force it in the application.

--- App/index.php
+++ App/index.php
@@ -1,5 +1,13 @@
 <?php
 
+// In case ingress controller doesn't do the redirection from http to https, do it ourselves.
+if( !(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') ) {
+  $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  header('HTTP/1.1 301 Moved Permanently');
+  header('Location: ' . $redirect);
+  exit();
+}
+
 // Initiate the session
 session_start();
 
